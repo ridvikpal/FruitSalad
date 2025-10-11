f13d774523ff50dc5a5e7de261b69243ff150820
Ridvik Pal <ridvikpal@gmail.com>
2025-10-11 19:58:40 -0400

    Updated PrimaryModel

--- models.py
+++ models.py
@@ -7,15 +7,17 @@ class PrimaryModel(nn.Module):
 
         self.name = "primary_model"
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv1 = nn.Conv2d(3, 16, 5)
-        self.conv2 = nn.Conv2d(16, 32, 5)
-        self.fc1 = nn.Linear(32 * 53 * 53, 32) # change this
-        self.fc2 = nn.Linear(32, 9) # change this
+        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.conv2 = nn.Conv2d(16, 32, 3)
+        self.conv3 = nn.Conv2d(32, 64, 3)
+        self.fc1 = nn.Linear(64 * 14 * 14, 32)
+        self.fc2 = nn.Linear(32, 5)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 53 * 53) # change this
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 64 * 14 * 14)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
