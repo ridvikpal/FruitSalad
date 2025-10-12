ca893d994952e6bb30c68c4fb1f0396e51bbecc8
Ridvik Pal <ridvikpal@gmail.com>
2025-10-11 20:02:46 -0400

    Updated the baseline model.

--- models.py
+++ models.py
@@ -26,5 +26,13 @@ class BaselineModel(nn.Module):
     def __init__(self):
         super(BaselineModel, self).__init__()
 
+        self.name = "baseline_model"
+        self.fc1 = nn.Linear(3 * 128 * 128, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 5)
+
     def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
